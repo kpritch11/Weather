db150dc7d901239e4c4805a91f2766f3a24ddb94
kurtispritchard <kpritch11@gmail.com>
2016-09-10 11:33:55 -0500

    Added axios for ajax requests. Created fetchWeather action.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
+import axios from 'axios';
+
 const API_Key = 'bfba5db7cce578048f894c823a59be50';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${APP_ID}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
